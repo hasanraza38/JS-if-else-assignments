e5cf2f9cb9a3dde5d89a7b9c6de66669cca1f7cf
hasanraza38 <92hasanraza689@gmail.com>
2024-03-08 02:32:12 +0500

    if else

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
 // Q/1
 
-var num= +prompt("enter Your number :");
-if(num % 3 === 0){
-    alert("The Number Is Divisible ");
+// var num= +prompt("enter Your number :");
+// if(num % 3 === 0){
+//     alert("The Number Is Divisible ");
 
-}
-else(
-    alert("The Number Is Not Divisible ")
+// }
+// else{
+//     alert("The Number Is Not Divisible ")
 
-)
+// }
+// Q/2
+// var num= +prompt("enter Your number :");
+// if(num % 2 === 0){
+//         alert("The Number Is Even ");
+    
+//     }
+//     else{
+//         alert("The Number Is odd ")
+//     }
+
+// Q/3
+
+// var num= +prompt("enter Your number :");
+// if(num >= 18){
+//     alert("Old Enough")
+// }else{
+//     alert("too Young")
+// }
+
+
+// Q/4
+
+// var Name = prompt("Enter Your Name :");
+
+// if(Name == hasan){
+// alert("HEYY HASAN WASSUP!!");
+// }else{
+// console.log(" ");
+// }
